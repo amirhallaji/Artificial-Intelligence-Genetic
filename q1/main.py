9b2e7b950fb9ef84a57ab49ded69b320a859cad9
amirhallaji <a.hallaji.b@gmail.com>
2021-03-30 10:01:48 +0430

    initial commit

---
+++ q1/main.py
@@ -0,0 +1,46 @@
+import random
+#
+# def calculateFinalValue(price: list):
+#     finalDetails = []
+#     for carIndex in range(0, len(price)):
+#         cols = [carIndex, price[carIndex][0]]
+#         finalValue = price[carIndex][1] - price[carIndex][2] * price[carIndex][3]
+#         cols.append(finalValue)
+#         finalDetails.append(cols)
+#     return finalDetails
+
+def initializePopulation():
+
+
+
+
+if __name__ == '__main__':
+    listOfAll = []
+    firstAsset = input()
+    firstAsset = int(firstAsset)
+
+    carCount = input(int)
+    carCount = int(carCount)
+
+    for i in range(0, carCount):
+        cols = []
+        carPrice = input()
+        carPrice = int(carPrice)
+        cols.append(carPrice)
+
+        earlyValue = input()
+        earlyValue = int(earlyValue)
+        cols.append(earlyValue)
+
+        distance = input()
+        distance = int(distance)
+        cols.append(distance)
+
+        decreaseRate = input() # enter 0 if not wanted.
+        decreaseRate = int(decreaseRate)
+        cols.append(decreaseRate)
+
+        listOfAll.append(cols)
+
+
+
