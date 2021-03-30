b8d235e4f0e0aa06a14fdec57f9909a269229cb7
amirhallaji <a.hallaji.b@gmail.com>
2021-03-30 15:56:22 +0430

    initial population implementation.

--- q1/main.py
+++ q1/main.py
@@ -1,14 +1,20 @@
 import random
 
-#
-# def calculateFinalValue(price: list):
-#     finalDetails = []
-#     for carIndex in range(0, len(price)):
-#         cols = [carIndex, price[carIndex][0]]
-#         finalValue = price[carIndex][1] - price[carIndex][2] * price[carIndex][3]
-#         cols.append(finalValue)
-#         finalDetails.append(cols)
-#     return finalDetails
+def calculateInitialPopulation(inputs: list):
+    initialPopulation = []
+    for i in range(0, len(inputs)):
+        cols = [i]
+        if len(inputs[i]) == 2:
+            cols.append(inputs[i][0])
+            cols.append(inputs[i][1])
+        else:
+            finalValue = inputs[i][1] - inputs[i][2] * inputs[i][3]
+            cols.append(inputs[i][0])
+            cols.append(finalValue)
+        initialPopulation.append(cols)
+    return initialPopulation
+
+
 
 def initializePopulation():
     initialPopulation = []
@@ -66,40 +72,21 @@ def chooseSurvivals(population: list):
     return survivals
 
 if __name__ == '__main__':
-    pop = initializePopulation()
 
-    for i in range(0, 5):
-        pop = crossover(pop)
-        pop = mutation(pop)
-        pop = chooseSurvivals(pop)
-    print(pop)
+    initialMoney = input()
+    initialMoney = int(initialMoney)
 
-    # listOfAll = []
-    # firstAsset = input()
-    # firstAsset = int(firstAsset)
-    #
-    # carCount = input(int)
-    # carCount = int(carCount)
-    #
-    # for i in range(0, carCount):
-    #     cols = []
-    #     carPrice = input()
-    #     carPrice = int(carPrice)
-    #     cols.append(carPrice)
-    #
-    #     earlyValue = input()
-    #     earlyValue = int(earlyValue)
-    #     cols.append(earlyValue)
-    #
-    #     distance = input()
-    #     distance = int(distance)
-    #     cols.append(distance)
-    #
-    #     decreaseRate = input() # enter 0 if not wanted.
-    #     decreaseRate = int(decreaseRate)
-    #     cols.append(decreaseRate)
-    #
-    #     listOfAll.append(cols)
+    # count of cars
+    N = input()
+    N = int(N)
+
+    listOfInputs = []
+    for i in range(0, N):
+        listOfInputs.append(list(map(int, input().split())))
+
+    print(listOfInputs)
+
+    print(calculateInitialPopulation(listOfInputs))
 
 
 
