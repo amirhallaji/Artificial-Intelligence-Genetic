7110854231e9b11816d9128adf9e94c407765746
amirhallaji <a.hallaji.b@gmail.com>
2021-04-01 20:05:30 +0430

    starting question2

--- q2/main.py
+++ q2/main.py
@@ -1,6 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.random as rand
+
+class Individual:
+
+    def __init__(self, degree):
+        coefficient = []
+    #-----------------------------
+
+    def calcFitness(self):
+        self.fitness = 0
+
+
 
 if __name__ == '__main__':
 
